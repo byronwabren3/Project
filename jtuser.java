4653e1a0d12060ea953948fcb9a44c2bd5ae18d9
byronwabren3 <114427268+byronwabren3@users.noreply.github.com>
2023-03-24 09:09:40 -0700

    Add files via upload

---
+++ jtuser.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logins;
+
+/**
+ *
+ * @author Admin
+ */
+class jtuser {
+
+    static void grabFocus() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
